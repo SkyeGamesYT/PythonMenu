3c185a968d6791451680830c47d7c5f4cd11b3f2
SkyeGamesYT <113136719+SkyeGamesYT@users.noreply.github.com>
2023-09-06 14:19:39 -0500

    Fixed

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 #Main.py
 
-
+#Credit, Keep this in
 print("All Credit to SkyeGamesYT")
 
+
+#Menu Function
+diff exit1()
+input("Press enter to exit")
+
+
+diff menu()
+print("-" * 50)
+x = input("Choice 1/nChoice 2/nChoice 3")
+print("-" * 50)
+if x == 1:
+  #Code for choice 1
+if x == 2:
+  #Code for choice 2
+if x == 3:
+  #Code for choice 3
+diff again():
+y = input("Print again? Y/N ")
+if y == "Y":
+  menu()
+if y == "N":
+  exit1()
+#Starting of function
