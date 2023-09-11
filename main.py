4f891a424234403564ea5a27939c856c0e9cda55
SkyeGamesYT <113136719+SkyeGamesYT@users.noreply.github.com>
2023-09-11 08:01:24 -0500

    Fixed

--- main.py
+++ main.py
@@ -9,18 +9,28 @@ def exit1():
 
 
 def menu():
-    print("-"*50)
-    x = input("Choice 1\nChoice 2\nChoice 3")
-    print("-"*50)
-    if x == "1":
-        #Code for choice 1
-    if x == "2":
-        #Code for choice 2
-    if x == "3":
+  print("-"*50)
+  x = input("Choice 1\nChoice 2\nChoice 3")
+  print("-"*50)
+  if x == "1":
+    print("Choice 1")
+    #Code for choice 1
+  if x == "2":
+    print("Choice 2")
+      #Code for choice 2
+  if x == "3":
+    print("Choice 3")
         #Code for choice 3
-  def again():
-    y = input("Print again? Y/N ")
-    if y == "Y":
-        menu()
-    if y == "N":
-        exit1()
+        
+def Playagain():
+          y = input("Print again? Y/N: ")
+          if y == "Y":
+            menu()
+            if y == "N":
+              exit()
+
+
+
+#Actual Menu code
+menu()
+Playagain()
